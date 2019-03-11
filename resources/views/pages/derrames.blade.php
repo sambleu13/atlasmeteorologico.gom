61ee1ec7321a45e056925682f9475c8d06fc018d
Cuauhtemoc <cuauhtemoc.silvav@gmail.com>
2019-03-11 00:29:20 -0600

    Update derrames.blade.php
    Se añadió breadcrumb

--- resources/views/pages/derrames.blade.php
+++ resources/views/pages/derrames.blade.php
@@ -9,6 +9,7 @@
     <!--main container -->
        <!--main container -->
 <div class="wrapper">
+{!! Breadcrumbs::render('derrames') !!}
      <div class="embed-container">
         <iframe   src="http://pronosticos.atmosfera.unam.mx/escenarios/" frameborder="0" allowfullscreen></iframe>
     </div>
