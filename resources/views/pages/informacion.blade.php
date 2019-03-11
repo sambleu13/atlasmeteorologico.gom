c8b9caaf455a36b2471e1041423e6f11881f9d12
Cuauhtemoc <cuauhtemoc.silvav@gmail.com>
2019-03-11 00:09:09 -0600

    Update informacion.blade.php
    Se añadió breadcrumb Información

--- resources/views/pages/informacion.blade.php
+++ resources/views/pages/informacion.blade.php
@@ -7,6 +7,7 @@
 @section('content')
     <!--main container -->
     <div class="wrapper">
+    {!! Breadcrumbs::render('informacion') !!}
         <div class="row">
             <div class="container-fluid">
                 <div class="col-sm-12 col-md-12 col-lg-12">
