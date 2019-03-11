a0a58850217cb6ee0ee43e6f633c464791345b98
Cuauhtemoc <cuauhtemoc.silvav@gmail.com>
2019-03-11 00:30:53 -0600

    Update humedad.blade.php
    Se añadió breadcrumb humedad

--- resources/views/pages/humedad.blade.php
+++ resources/views/pages/humedad.blade.php
@@ -9,6 +9,7 @@
 
     <!--main container -->
      <div class="wrapper">
+     {!! Breadcrumbs::render('humedad') !!}
         <div class="row">
             <div class="container-fluid">
                 <div class="col-lg-12">
